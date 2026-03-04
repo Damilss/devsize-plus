f5ff7609193f16ad54b379e5aeadf1b6e244f30e
Damilss <milii76@outlook.com>
2026-03-03 19:04:23 -0800

    app

---
+++ devsize-plus/devsize-plus/devsize_plusApp.swift
@@ -0,0 +1,18 @@
+//
+//  devsize_plusApp.swift
+//  devsize-plus
+//
+//  Created by emilio on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct devsize_plusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.automatic)
+    }
+}
